5b8f3a0d17571ac87a20ea28cade3e3afd9b607a
kurtis <davidsku18@up.edu>
2017-02-17 13:46:44 -0800

    HW4d Start

---
+++ app/src/main/java/edu/up/citadels/DistrictCardCost.java
@@ -0,0 +1,65 @@
+package edu.up.citadels;
+
+/**
+ * Created by kurtisdavidson on 2/12/17.
+ */
+
+public class DistrictCardCost{
+
+    // 5 Tavern	1 green
+    // 4 Market	2 green
+    // 3 Trading Post	2 green
+    // 3 Docks	3 green
+    // 3 Harbor	4 green
+    // 2 Town Hall	5 green
+    // 3 Temple	1 blue
+    // 3 Church	2 blue
+    // 3 Monastery	3 blue
+    // 2 Cathedral	5 blue
+    // 3 Watchtower	1 red
+    // 3 Prison	2 red
+    // 3 Battlefield	3 red
+    // 2 Fortress	5 red
+    // 5 Manor	3 yellow
+    // 4 Castle	4 yellow
+    // 3 Palace	5 yellow
+    // 1 Haunted City2 purple
+    // 2 Keep	3 purple
+    // 1 Laboratory	5 purple
+    // 1 Smithy	5 purple
+    // 1 Graveyard	5 purple
+    // 1 Observatory	5 purple
+    // 1 School of Magic	6 purple
+    // 1 Library	6 purple
+    // 1 Great Wall	6 purple
+    // 1 University	8 purple
+    // 1 Dragon Gate	8 purple
+    protected final int tavern = 1;
+    protected final int market = 2;
+    protected final int tradingPost = 2;
+    protected final int docks = 3;
+    protected final int harbor = 4;
+    protected final int townHall = 5;
+    protected final int temple = 1;
+    protected final int church = 2;
+    protected final int monastery = 3;
+    protected final int cathedral = 5;
+    protected final int watchtower= 1;
+    protected final int prison= 2;
+    protected final int battlefield = 3;
+    protected final int fortress = 5;
+    protected final int manor = 3;
+    protected final int castle = 4;
+    protected final int palace = 5;
+    protected final int hauntedCity = 2;
+    protected final int keep = 3;
+    protected final int laboratory = 5;
+    protected final int smithy = 5;
+    protected final int graveyard = 5;
+    protected final int observatory = 5;
+    protected final int schoolOfMagic = 6;
+    protected final int library = 6;
+    protected final int greatWall = 6;
+    protected final int university = 8;
+    protected final int dragonGate = 8;
+}
--- app/src/main/java/edu/up/citadels/MainActivity.java
+++ app/src/main/java/edu/up/citadels/MainActivity.java
@@ -2,27 +2,100 @@ package edu.up.citadels;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+
 public class MainActivity extends AppCompatActivity{
 
+    private ImageButton player1_Card1;
+    private ImageButton player1_Card2;
+    private boolean p1_Card1Bool = true; // boolean for player1_Card1
+    private boolean p1_Card2Bool = true; // boolean for player1_Cart2
+    private ImageButton p1_D1;
+    private ImageButton p1_D2;
+    private ImageButton p1_D3;
+    private ImageButton p1_D4;
+    private ImageButton p1_D5;
+    private ImageButton p1_D6;
+    private ImageButton p1_D7;
+    private ImageButton p1_D8;
+    private boolean p = true;
+
     private Spinner player1HandSpinner;
+
     List<String> player1DistrictsCards = new ArrayList<String>();
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        player1_Card1 = (ImageButton) findViewById(R.id.player1_Card1);
+        player1_Card2 = (ImageButton) findViewById(R.id.player1_Card2);
+
+        p1_D1 = (ImageButton) findViewById(R.id.p1_D1);
+        p1_D2 = (ImageButton) findViewById(R.id.p1_D2);
+        p1_D3 = (ImageButton) findViewById(R.id.p1_D3);
+        p1_D4 = (ImageButton) findViewById(R.id.p1_D4);
+
+        String[] p1Hand = getResources().getStringArray(R.array.p1Hand);
+
         player1HandSpinner = (Spinner) findViewById(R.id.player1HandSpinner);
 
+        player1_Card1.setOnClickListener(new View.OnClickListener()
+        {
+            Drawable defaultImageColor = player1_Card1.getBackground(); // gets default background color
+            @Override
+            public void onClick(View view)
+            {
+                if(p1_Card1Bool) {
+                    player1_Card1.setBackgroundColor(getResources().getColor(R.color.blackButtonPressColor));
+                }
+                else{
+                    player1_Card1.setBackgroundDrawable(defaultImageColor);
+                }
+                p1_Card1Bool=!p1_Card1Bool;
+            }
+        });
+
+        player1_Card2.setOnClickListener(new View.OnClickListener()
+        {
+            Drawable defaultImageColor = player1_Card2.getBackground(); // gets default background color
+            @Override
+            public void onClick(View view)
+            {
+                if(p1_Card2Bool) {
+                    player1_Card2.setBackgroundColor(getResources().getColor(R.color.blackButtonPressColor));
+                }
+                else{
+                    player1_Card2.setBackgroundDrawable(defaultImageColor);
+                }
+                p1_Card2Bool=!p1_Card2Bool;
+            }
+        });
+
+        p1_D1.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                
+            }
+        });
+
+
+
     }
+
+
 
     private class SpinnerLister implements View.OnClickListener {
 
