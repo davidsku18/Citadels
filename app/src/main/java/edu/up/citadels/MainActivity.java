0028b2779d6be6390c1a86fc52683d5781e0ebfc
gavinlow <gavin.pe.low@gmail.com>
2017-02-17 15:05:37 -0800

    Added card info text view which shows the following: Card name Card value Card color
    This information is displayed when the card is clicked and disappears when the card is clicked again

--- app/src/main/java/edu/up/citadels/MainActivity.java
+++ app/src/main/java/edu/up/citadels/MainActivity.java
@@ -10,6 +10,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 
@@ -27,7 +28,11 @@ public class MainActivity extends AppCompatActivity{
     private ImageButton p1_D6;
     private ImageButton p1_D7;
     private ImageButton p1_D8;
-    private boolean p = true;
+    private TextView cardInfo; //initializes cardInfo TextView
+    private boolean d1_Info = false; //initializes d1_Info Boolean
+    private boolean d2_Info = false; //initializes d2_Info Boolean
+    private boolean d3_Info = false; //initializes d3_Info Boolean
+    private boolean d4_Info = false; //initializes d4_Info Boolean
 
     private Spinner actionSpinner;
     private Spinner player1HandSpinner;
@@ -47,7 +52,9 @@ public class MainActivity extends AppCompatActivity{
         p1_D3 = (ImageButton) findViewById(R.id.p1_D3);
         p1_D4 = (ImageButton) findViewById(R.id.p1_D4);
 
-        String[] p1Hand = getResources().getStringArray(R.array.p1Hand);
+        cardInfo = (TextView)findViewById(R.id.helpText); //sets cardInfo to the helpText TextView
+
+        //String[] p1Hand = getResources().getStringArray(R.array.p1Hand);
 
         player1HandSpinner = (Spinner) findViewById(R.id.player1HandSpinner);
         actionSpinner = (Spinner) findViewById(R.id.actionSpinner);
@@ -84,12 +91,27 @@ public class MainActivity extends AppCompatActivity{
             }
         });
 
+
+//////////////////////District Card Info Text Views///////////////////////////////////
+        //Click on the card to display useful info about the card
+        //Click on the card again to remove displayed information
+        //The card with the displayed information must be clicked to show the next card's information
         p1_D1.setOnClickListener(new View.OnClickListener()
         {
             @Override
             public void onClick(View view)
             {
+                if(cardInfo.getText() == "")
+                {
+                    cardInfo.setText("Manor\nValue: 4\nColor: Gold"); //displays card info (name, value, color)
+                    d1_Info = true;
 
+                }
+               else if(d1_Info == true)
+                {
+                    cardInfo.setText("");
+                    d1_Info = false;
+                }
             }
         });
 
@@ -98,6 +120,16 @@ public class MainActivity extends AppCompatActivity{
             @Override
             public void onClick(View view)
             {
+                if(cardInfo.getText() == "")
+                {
+                    cardInfo.setText("Town Hall\nValue: 3\nColor: Gold");
+                    d2_Info = true;
+                }
+                else if( d2_Info == true)
+                {
+                    cardInfo.setText("");
+                    d2_Info = false;
+                }
 
             }
         });
@@ -107,7 +139,17 @@ public class MainActivity extends AppCompatActivity{
             @Override
             public void onClick(View view)
             {
+                if(cardInfo.getText() == "")
+                {
+                    cardInfo.setText("Church\nValue: 2\nColor: Blue");
+                    d3_Info = true;
 
+                }
+                else if( d3_Info == true)
+                {
+                    cardInfo.setText("");
+                    d3_Info = false;
+                }
             }
         });
 
@@ -116,7 +158,17 @@ public class MainActivity extends AppCompatActivity{
             @Override
             public void onClick(View view)
             {
+                if(cardInfo.getText() == "")
+                {
+                    cardInfo.setText("Battlefield\nValue: 3\nColor: Red");
+                    d4_Info = true;
 
+                }
+                else if(d4_Info == true)
+                {
+                    cardInfo.setText("");
+                    d4_Info = false;
+                }
             }
         });
 
