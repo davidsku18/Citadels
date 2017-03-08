15ff3b6dd0bf7a647dd65c760c8f1c40aaa719f6
kurtis <davidsku18@up.edu>
2017-03-07 17:08:48 -0800

    Actions package added and moved action classes

---
+++ app/src/main/java/edu/up/citadels/Actions/EndTurn.java
@@ -0,0 +1,21 @@
+package edu.up.citadels.Actions;
+
+import ActionMsg.GameAction;
+import player.GamePlayer;
+
+/**
+ * Created by gavinlow on 3/6/17.
+ */
+
+public class EndTurn extends GameAction
+{
+    /**
+     * constructor for game.GameAction
+     *
+     * @param player the player who created the action
+     */
+    public EndTurn(GamePlayer player)
+    {
+        super(player);
+    }
+}
