bdef639c235f88d1f5e034300c87253899d7b96c
gavinlow <gavin.pe.low@gmail.com>
2017-03-23 21:03:16 -0700

    Added test for score setters and getters

--- app/src/test/java/edu/up/citadels/CitadelsGameStateTest.java
+++ app/src/test/java/edu/up/citadels/CitadelsGameStateTest.java
@@ -20,4 +20,36 @@ public class CitadelsGameStateTest
         CitadelsGameState cgs = new CitadelsGameState();
 
     }
+
+    @Test
+    public void testGetPlayerScore() throws Exception
+    {
+        CitadelsGameState gameState = new CitadelsGameState();
+        int testP1Score = gameState.getP1Score();
+        int testP2Score = gameState.getP2Score();
+        int testP3Score = gameState.getP3Score();
+        assertEquals(testP1Score, 0, 0.00);
+        assertEquals(testP2Score, 0, 0.00);
+        assertEquals(testP3Score, 0, 0.00);
+
+
+    }
+
+    @Test
+    public void testSetPlayerScore() throws Exception
+    {
+        CitadelsGameState gameState = new CitadelsGameState();
+        gameState.setP1Score(6);
+        gameState.setP2Score(5);
+        gameState.setP3Score(8);
+        int testP1Score = gameState.getP1Score();
+        int testP2Score = gameState.getP2Score();
+        int testP3Score = gameState.getP3Score();
+        assertEquals(testP1Score, 6, 0.00);
+        assertEquals(testP2Score, 5, 0.00);
+        assertEquals(testP3Score, 8, 0.00);
+
+
+    }
+
 }
