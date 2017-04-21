ffc0a9530d8d97c0fa3140facc950ad98833ebfc
Victor <nguyenv19@up.edu>
2017-04-21 13:41:21 -0700

    Commit For New Branch for AI

--- app/src/main/java/edu/up/citadels/citadels/CitadelsComputerPlayerSmart.java
+++ app/src/main/java/edu/up/citadels/citadels/CitadelsComputerPlayerSmart.java
@@ -52,9 +52,11 @@ public class CitadelsComputerPlayerSmart extends GameComputerPlayer
         if(myPlayer == 1)
         {
             game.sendAction(new CitadelsBuildDistrictCard(this, (CitadelsDistrictCard) savedState.getP1Hand().get(0)));
-        }else if(myPlayer == 2)
+        }
+        else if(myPlayer == 2)
         {
-            game.sendAction(new CitadelsBuildDistrictCard(this, (CitadelsDistrictCard) savedState.getP2Hand().get(0)));
+
+                game.sendAction(new CitadelsBuildDistrictCard(this, (CitadelsDistrictCard) savedState.getP2Hand().get(0)));
 
         }else if(myPlayer == 3)
         {
